fc1453056794fec1e6242bd204d4cb81b6fce4a6
sontinagina <sontinagina11@gmail.com>
2021-12-13 00:59:05 +0530

    first commit

---
+++ Files/App.js
@@ -0,0 +1,12 @@
+function display(val){
+  document.getElementById("input").value+=val;
+
+}
+function solve(){
+  let x=document.getElementById("input").value;
+  let y=eval(x)
+  document.getElementById("input").value=y;
+}
+function clr(){
+  document.getElementById("input").value="";
+}
